eab99f3f4a722d90e0ac87af1b65dcea48d61438
Isaiah Fasoldt <ifasoldt@gmail.com>
2016-08-16 11:12:46 -0400

    some controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  helper_method :current_user
+
+  def require_user
+    redirect_back(fallback_location: root_path, flash: {danger: "You need to be logged in to do that"}) unless current_user
+  end
+
+  def current_user
+    if session[:email]
+     @current_user ||= User.find_by(email: session[:email])
+    end
+    @current_user
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+resources :events
+resources :users, except:[:index, :edit, :create, :new]
+resources :user_sessions, only: [:destroy]
+resources :applications, only: [:create, :destroy]
+resources :thumbs, only: [:create]
+
+root 'welcome#index'
+
+# sign_up
+post '/api/users' => 'users#create'
+
+# sign_in
+post '/api/sign_in' => 'user_sessions#create'
+
 end
