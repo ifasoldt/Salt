515beb27413907df43c157976a46d464d20634c5
Isaiah Fasoldt <ifasoldt@gmail.com>
2016-08-17 14:11:26 -0400

    validates the guest number must be a number

--- app/models/application.rb
+++ app/models/application.rb
@@ -2,5 +2,6 @@ class Application < ApplicationRecord
   belongs_to :user
   belongs_to :event
   validates :quantity, presence: true
+  validates :quantity, numericality: {only_integer: true}
   validates :message, length: {maximum: 1000}, allow_blank: true
 end
