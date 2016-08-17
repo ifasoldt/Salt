849e1cf35c8ced96c9efcf40b985b585eadaf4b5
Isaiah Fasoldt <ifasoldt@gmail.com>
2016-08-17 14:31:33 -0400

    custom validation message for quantity of guests

--- app/models/application.rb
+++ app/models/application.rb
@@ -2,6 +2,6 @@ class Application < ApplicationRecord
   belongs_to :user
   belongs_to :event
   validates :quantity, presence: true
-  validates :quantity, numericality: {only_integer: true}
+  validates :quantity, numericality: {only_integer: true, message: 'of guests must be specified'}
   validates :message, length: {maximum: 1000}, allow_blank: true
 end
