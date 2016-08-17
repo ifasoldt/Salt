dcb0b34b7c4fdbb60d2d2051a4ac1daba6c1a3ba
Isaiah Fasoldt <ifasoldt@gmail.com>
2016-08-17 13:40:06 -0400

    applications now nested inside events

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -1,7 +1,8 @@
 class ApplicationsController < ApplicationController
+  before_action :require_user only: [:create]
 
   def create
-    @application = Application.new(application_params)
+    @application = Application.new(application_params.merge(user_id: current_user.id))
     if @application.save
       render json: @application
     else
@@ -21,6 +22,6 @@ class ApplicationsController < ApplicationController
   private
 
   def application_params
-      params.permit(:event_id, :user_id, :quantity, :message)
+      params.permit(:event_id, :quantity, :message)
   end
 end
