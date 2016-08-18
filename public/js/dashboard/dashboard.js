edae46b7f3aa53b45264fddfb44527610ccbbba8
Jake Willingham <jakewillingham789@gmail.com>
2016-08-18 16:13:00 -0400

    made changes to profile edit

--- public/js/dashboard/dashboard.js
+++ public/js/dashboard/dashboard.js
@@ -4,19 +4,22 @@ var current_id = profile.getAttribute('data-id')
 // update profile
 document.getElementById('btn_submit_profile').addEventListener('click', function () {
   removeErrors()
-  var formFields = {
-    first_name: document.getElementById('first_name_profile').value,
-    last_name: document.getElementById('last_name_profile').value,
-    email: document.getElementById('email_profile').value,
-    date_of_birth: document.getElementById('date_of_birth_profile').value,
-    phone: document.getElementById('phone_profile').value,
-    street: document.getElementById('street_profile').value,
-    city: document.getElementById('city_profile').value,
-    state: document.getElementById('state_profile').value,
-    zip: document.getElementById('zip_profile').value
+  var data = new FormData()
+  var pics = document.getElementById('images_files_profile').files.length;
+  for (var x = 0; x < pics; x++) {
+    data.append('images_files[]', document.getElementById('images_files_profile').files[x]);
   }
+  data.append('first_name', document.getElementById('first_name').value)
+  data.append('last_name', document.getElementById('last_name').value)
+  data.append('email', document.getElementById('email').value)
+  data.append('date_of_birth', document.getElementById('date_of_birth').value)
+  data.append('phone', document.getElementById('phone').value)
+  data.append('street', document.getElementById('street').value)
+  data.append('city', document.getElementById('city').value)
+  data.append('state', document.getElementById('state').value)
+  data.append('zip', document.getElementById('zip').value)
 
-  fetchApi('PATCH',`/api/users/${current_id}`, formFields, function (response, statusCode) {
+  fetchApiImages('PATCH',`/api/users/${current_id}`, data, function (response, statusCode) {
     if (statusCode >= 200 && statusCode < 300) {
       location.reload()
     }
@@ -46,20 +49,6 @@ document.getElementById('btn_event_submit').addEventListener('click', function (
   data.append('food', document.getElementById('food').value)
   data.append('description', document.getElementById('description').value)
 
-
-  // data.append('user', 'hubot')
-  // var formFields = {
-  //   title: document.getElementById('title').value,
-  //   date: document.getElementById('date').value,
-  //   time: document.getElementById('time').value,
-  //   guest_limit: document.getElementById('guest_limit').value,
-  //   unlimited_guests: document.getElementById('unlimited_guests').checked,
-  //   allow_children: document.getElementById('allow_children').checked,
-  //   alcohol_allowed: document.getElementById('alcohol_allowed').checked,
-  //   filter_guests: document.getElementById('filter_guests').checked,
-  //   food: document.getElementById('food').value,
-  //   description: document.getElementById('description').value
-  // }
   fetchApiImages('POST', '/api/events', data, function (response, statusCode) {
     if (statusCode >= 200 && statusCode < 300) {
       console.log(response)
