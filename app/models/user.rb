0949ff11944db535366e1c514059a96ab51516fa
Isaiah Fasoldt <ifasoldt@gmail.com>
2016-08-19 13:02:25 -0400

    phone #s now validate correct format

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
   has_one :address, as: :addressable, dependent: :destroy
-  accepts_nested_attributes_for :address
+  # doesn't validate presence of address if all_blank
+  accepts_nested_attributes_for :address, reject_if: :all_blank
   has_many :hosted_events, class_name: 'Event', foreign_key: 'host_id', dependent: :destroy
   has_many :events, through: :applications
   has_many :applications, dependent: :destroy
@@ -12,13 +13,13 @@ class User < ApplicationRecord
 
 
   validates :first_name, :last_name, :email, :date_of_birth, presence: true
-  validates :first_name, :last_name, format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" }
+  validates :first_name, :last_name, format: { with: /([ \u00c0-\u01ffa-zA-Z'\-])+/, message: "only allows letters" }
   validates :email, format: {with: /\A\S+@.+\.\S+\z/, message: "must be a valid email"}
   validates :email, uniqueness: true
   validates :password, length: {minimum: 6}, on: :create
   validates :description, length: {maximum: 2000}, allow_nil: true
-  validates :phone, numericality: {only_integer: true}, allow_blank: true
-  validates :phone, length: {is: 10, message: 'must be in 1231231234 format'}, allow_blank: true
+  validates :phone, format: {with: /\A\d{3}-\d{3}-\d{4}\z/}, allow_blank: true
+  validates :phone, length: {is: 12, message: 'must be in 123-123-1234 format'}, allow_blank: true
   validates_date :date_of_birth, :before => lambda { 18.years.ago }, :before_message => "must be at least 18 years ago."
   # see note below on date_must_be_formatted_correctly. Also if and unless seem to be opposite what they should be.
   validates_date :date_of_birth, :after => lambda {125.years.ago}, :after_message => "suggests that you are dead.", unless: :special_date
