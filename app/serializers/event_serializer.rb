6dca104f5c994ee78e9f55ea8bfc41534cd65a33
Isaiah Fasoldt <ifasoldt@gmail.com>
2016-09-01 10:56:08 -0400

    same stuff

--- app/serializers/event_serializer.rb
+++ app/serializers/event_serializer.rb
@@ -40,7 +40,7 @@ class EventSerializer < ActiveModel::Serializer
       marker.infowindow "<div class='infoWindow'>
       <div class='eventInfoTitle'><a href='/events/#{object.id}'}>#{object.title}</a></div>
         <div class='eventInfoImage'>
-          <img class='img-responsive' width='200' height='200' src='#{(Refile.attachment_url(object.images.first, :file, :fit, 400, 400)) || "/assets/no_event_image.jpg"}' />
+          <img class='img-responsive' width='200' height='200' src='#{(Refile.attachment_url(object&.images&.first, :file, :fit, 400, 400)) || "/assets/no_event_image.jpg"}' />
         </div>
         <div class='eventInfoDateTime'>#{object.formatted_date} at #{object.formatted_time}</div>
         <div class='eventInfoGuestLimit'> Guest Limit #{object.guest_limit || "none"} Spots Left: #{object.spots_left} </div>
